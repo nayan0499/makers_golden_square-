e48773678cefead63ecef4fc69a3ae1ea7a67976
nayan0499 <nayangrg.uni@gmail.com>
2023-07-31 12:10:01 +0100

    Add test to remove all vowels

---
+++ phase_2/07_intermezzo_debugging_2/lib/vowel_remover.py
@@ -0,0 +1,15 @@
+class VowelRemover:
+    def __init__(self, text):
+        self.text = text
+        self.vowels = ["a", "e", "i", "o", "u"]
+
+    def remove_vowels(self):
+        i = len(self.text) -1 
+        while i >= 0:
+            if self.text[i].lower() in self.vowels:
+                self.text = self.text[:i] + self.text[i+1:]
+            i -= 1
+        return self.text
+
+remover = VowelRemover("aeiou")
+result = remover.remove_vowels()
